2d8b7d54a43a92e3b7612775e4b19303e1b8582e
Matheus Carrasco <159260622+matheuscarrasco@users.noreply.github.com>
2024-07-31 11:31:51 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+function conteudo1() {
+    document.getElementById("conteudo").innerHTML = "<P>A Estátua da Liberdade não ficou em 1° lugar, porque mais do que representar Nova York, a Estátua da Liberdade talvez seja o grande ponto turístico que representa os Estados Unidos. </P>";
+    document.getElementById("imagem").innerHTML = "<img src='Estatua.jpg'>"
+}
+
+function conteudo2() {
+    document.getElementById("conteudo").innerHTML = "<P>De dia e de noite. Visite a Times Square. Parece besteira citar a Times Square como ponto turístico, mas muitos leitores esquecem de incluir o cruzamento mais famoso do mundo no roteiro de viagem em Nova York. A Times Square é um cruzamento entre a Broadway e a 42nd Street.  </P>"
+    document.getElementById("imagem").innerHTML = "<img src='Times.jpg'>"
+}
+
+function conteudo3() {
+    document.getElementById("conteudo").innerHTML = "<P>A ponte do Brooklyn é sem dúvidas um dos pontos turísticos mais marcantes de Nova York. Seja a pé, de bicicleta ou então de carro, cruze a ponte do Brooklyn é uma grande emoção.</P>";
+    document.getElementById("imagem").innerHTML = "<img src='Ponte.jpg'>"
+}
+
+function conteudo4() {
+    document.getElementById("conteudo").innerHTML = "<P>Aqui já passaram King Kong, presidentes, atrizes e atores de Hollywood e milhares de turistas, o Empire State Building é realmente um grande ícone de Nova York.  O Empire State Building foi inaugurado em 1931 e por 40 anos foi o prédio mais alto do mundo.</P>";
+    document.getElementById("imagem").innerHTML = "<img src='Empire.jpg'>"
+}
